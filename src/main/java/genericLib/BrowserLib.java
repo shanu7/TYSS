2e60f41b8bb2c4d27a8887b954d21ccd4e4bd724
shanu <shanu7>
2016-12-24 10:51:49 +0530

    my first msg

---
+++ src/main/java/genericLib/BrowserLib.java
@@ -0,0 +1,24 @@
+package genericLib;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserLib {
+	private WebDriver driver;
+
+	public WebDriver getBrowser() {
+		if (Constants.BROWSER.equalsIgnoreCase("chrome")) {
+
+		} else if (Constants.BROWSER.equalsIgnoreCase("ie")) {
+
+		} else if (Constants.BROWSER.equalsIgnoreCase("firefox")) {
+
+		} else if (Constants.BROWSER.equalsIgnoreCase("Opera")) {
+
+		} else if (Constants.BROWSER.equalsIgnoreCase("safari")) {
+
+		} else {
+			System.err.println("Sorry! No Such Browser");
+		}
+		return driver;
+	}
+}
---
+++ src/main/java/genericLib/Constants.java
@@ -0,0 +1,8 @@
+package genericLib;
+
+public interface Constants {
+	
+	String URL = "https://www.flipkart.com/";
+	String BROWSER = "ie";    
+
+}
